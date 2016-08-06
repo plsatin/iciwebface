c6f0a26f1aff3fe84ab6e40ea7dfdb046e70f679
Павел Сатин <pslater.ru@gmail.com>
2016-08-06 16:22:44 +0500

    Edit icons

--- config.php
+++ config.php
@@ -11,6 +11,8 @@ define('ICINGA2_HOST', '78.24.216.120');
 $username = "root";
 $password = "626589b6ba394d27";
 
+
+
 define('WWW_HOST_PATH', '/iciwebface');
 
 //ini_set( "display_errors", true );
